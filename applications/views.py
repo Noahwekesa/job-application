e1da2950f79b4215188b627c88b058ca5235c686
noahwekesa <noahwekesa0@gmail.com>
2024-02-12 16:40:24 +0300

    try

--- applications/views.py
+++ applications/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, render
 from .services import get_all_rows
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+"""
 
 def index(request):
     applications = get_all_rows("Application")
@@ -7,3 +14,5 @@ def index(request):
         "applications": applications
     }
     return render(request, 'index.html',context)
+
+"""
