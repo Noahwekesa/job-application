c392da7d2aa205a773ddf99de5b084a0ec2bc236
noahwekesa <noahwekesa0@gmail.com>
2024-02-16 18:18:03 +0300

    commit

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class Application(models.Model):
+    positon =  models.CharField(max_length=50)
--- applications/views.py
+++ applications/views.py
@@ -1,18 +1,8 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import render
 from .services import get_all_rows
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-
-"""
-
-def index(request):
     applications = get_all_rows("Application")
-    context = {
-        "applications": applications
-    }
-    return render(request, 'index.html',context)
-
-"""
+    context = {"applications": applications}
+    return render(request, "index.html", context)
